9d47b651c3a29319af5322e287bb4e64ee72a3c9
Juan Martin Vazquez <juavazquez@frba.utn.edu.ar>
2022-09-24 19:50:42 -0300

    Segundo commit, cambio de proyecto :)

--- main.js
+++ main.js
@@ -1,22 +1,54 @@
-const partido = () => {
-    let equipo1 = prompt("Ingrese el equipo local");
-    let equipo2 = prompt('Ingrese el equipo visitante');
+const pedido = () => {
+    let pizza = prompt("Que pizza desea llevar?");
 
-    let golesEquipo1 = parseInt(prompt('Ingrese los goles del equipo local'));
-    let golesEquipo2 = parseInt(prompt('Ingrese los goles del equipo visitante'));
-
-    if (golesEquipo1 > golesEquipo2) {
-        alert(`El ganador del encuentro es ${equipo1.toUpperCase()}`);
-        return equipo1;
+    let tamanioPizza = prompt('Que tamaño? [P => (porcion) / C => (chica) / G => (grande)]');
+    let tamPizza;
+    if (tamanioPizza == 'P' || tamanioPizza == 'p' || tamanioPizza == 'C' || tamanioPizza == 'c' || tamanioPizza == 'G' || tamanioPizza == 'g') {
+        tamPizza = queTamanioEs(tamanioPizza);
     } else {
-        alert(`El ganador del encuentro es ${equipo2.toUpperCase()}`);
-        return equipo2;
+        alert('No es un tamaño valido, por favor recarga la pagina e intenta de nuevo :(');
+        return;
+    }
+
+    let cantPizza = parseInt(prompt('Que cantidad?'));
+
+    let pedidoOK = prompt(`Usted pidio ${cantPizza} ${tamPizza} de ${pizza}, es correcto? [S/N]`);
+    
+    if (pedidoOK == 'S' || pedidoOK == 's') {
+        alert('Su pedido se ingreso correctamente :)');
+        return `Pedido => ${cantPizza} ${tamPizza} de ${pizza}`;
+    } else {
+        alert('No pudimos ingresar su pedido, por favor recargue la pagina e intente de nuevo :(');
     }
 }
 
-let cantPartidos = parseInt(prompt('Cuantos partidos queres jugar?'));
+const queTamanioEs = (tam) => {
+    switch (tam) {
+        case 'P':
+            return 'porcion';
+        case 'p':
+            return 'porcion';
+        case 'C':
+            return 'chica';
+        case 'c':
+            return 'chica';
+        case 'G':
+            return 'grande';
+        case 'g':
+            return 'grande';
+    }
+}
 
-for (let i = 0; i < cantPartidos; i++) {
-    alert(`Arranca el partido numero ${i + 1}`);
-    partido();
+// MAIN
+
+let cantPedidos = parseInt(prompt('Cuantos pedidos quiere realizar?'));
+
+for (let i = 0; i < cantPedidos; i++) {
+    alert(`Pedido numero ${i + 1}`);
+    let pedidoString = pedido();
+    if (typeof(pedidoString) == 'undefined') {
+        console.log('Se rompio, porfa recarga la pagina :(');
+        break;
+    }
+    console.log(pedidoString);
 }
